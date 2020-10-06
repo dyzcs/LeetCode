06c86925c0bebedbb577ab50b08d65f0dc6ffbbe
dyzcs <deng.kinger@gmail.com>
2020-10-06 18:09:10 +0800

    Update Offer_07.java

--- src/main/java/com/deng/offer/Offer_07.java
+++ src/main/java/com/deng/offer/Offer_07.java
@@ -1,5 +1,8 @@
 package com.deng.offer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2020/10/6.
  *
@@ -8,22 +11,61 @@ package com.deng.offer;
 public class Offer_07 {
     public static void main(String[] args) {
         SolutionOffer07 solutionOffer07 = new SolutionOffer07();
-        int[] preorder = {3,9,20,15,7};
-        int[] inorder = {9,3,15,20,7};
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
         SolutionOffer07.TreeNode treeNode = solutionOffer07.buildTree(preorder, inorder);
+        System.out.println(treeNode);
     }
 }
 
-class SolutionOffer07{
+class SolutionOffer07 {
     static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode(int x) { val = x; }
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        int length = preorder.length;
+        for (int i = 0; i < length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return buildTree(preorder, 0, length - 1, inorder, 0, length - 1, indexMap);
+    }
 
-        return null;
+    public TreeNode buildTree(int[] preorder, int preorderStart, int preorderEnd, int[] inorder, int inorderStart, int inorderEnd, Map<Integer, Integer> indexMap) {
+        if (preorderStart > preorderEnd) {
+            return null;
+        }
+        int rootVal = preorder[preorderStart];
+        TreeNode root = new TreeNode(rootVal);
+        if (preorderStart != preorderEnd) {
+            int rootIndex = indexMap.get(rootVal);
+            int leftNodes = rootIndex - inorderStart, rightNodes = inorderEnd - rootIndex;
+            TreeNode leftSubtree =
+                    buildTree(preorder, preorderStart + 1, preorderStart + leftNodes, inorder, inorderStart, rootIndex - 1, indexMap);
+            TreeNode rightSubtree =
+                    buildTree(preorder, preorderEnd - rightNodes + 1, preorderEnd, inorder, rootIndex + 1, inorderEnd, indexMap);
+            root.left = leftSubtree;
+            root.right = rightSubtree;
+        }
+        return root;
     }
 }
