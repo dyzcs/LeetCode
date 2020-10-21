6e489eeccc6ff50c6887b084c0d00ac958ec9a53
dyzcs <deng.kinger@gmail.com>
2020-10-21 09:10:52 +0800

    Update Offer_33.java

--- src/main/java/com/deng/offer/Offer_33.java
+++ src/main/java/com/deng/offer/Offer_33.java
@@ -1,14 +1,34 @@
 package com.deng.offer;
 
+import java.util.Stack;
+
 /**
  * Created by Administrator on 2020/10/20.
  */
 public class Offer_33 {
     public static void main(String[] args) {
-
+        int[][] arr = {{1, 6, 3, 2, 5}, {1, 3, 2, 6, 5}};
+        SolutionOffer33 solutionOffer33 = new SolutionOffer33();
+        for (int[] tmp : arr) {
+            boolean b = solutionOffer33.verifyPostorder(tmp);
+            System.out.println(b);
+        }
     }
 }
 
 class SolutionOffer33 {
-
+    public boolean verifyPostorder(int[] postorder) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) {
+                return false;
+            }
+            while (!stack.isEmpty() && stack.peek() > postorder[i]) {
+                root = stack.pop();
+            }
+            stack.add(postorder[i]);
+        }
+        return true;
+    }
 }
