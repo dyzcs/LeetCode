499ffe6cb5a359da0beab7499721e4e0c67902d0
dyzcs <deng.kinger@gmail.com>
2020-10-24 09:55:00 +0800

    Update LeetCode_1024.java

--- src/main/java/com/deng/lc/LeetCode_1024.java
+++ src/main/java/com/deng/lc/LeetCode_1024.java
@@ -7,7 +7,24 @@ package com.deng.lc;
  */
 public class LeetCode_1024 {
     public static void main(String[] args) {
+        int[][] arr1 = {{0, 2}, {4, 6}, {8, 10}, {1, 9}, {1, 5}, {5, 9}};
+        int t1 = 10;
 
+        int[][] arr2 = {{0, 1}, {1, 2}};
+        int t2 = 5;
+
+        int[][] arr3 = {{0, 1}, {6, 8}, {0, 2}, {5, 6}, {0, 4}, {0, 3},
+                {6, 7}, {1, 3}, {4, 7}, {1, 4}, {2, 5}, {2, 6}};
+        int t3 = 10;
+
+        int[][] arr4 = {{0, 4}, {2, 8}};
+        int t4 = 5;
+
+        SolutionLC1024 solutionLC1024 = new SolutionLC1024();
+        System.out.println(solutionLC1024.videoStitching(arr1, t1));
+        System.out.println(solutionLC1024.videoStitching(arr2, t2));
+        System.out.println(solutionLC1024.videoStitching(arr3, t3));
+        System.out.println(solutionLC1024.videoStitching(arr4, t4));
     }
 }
 
