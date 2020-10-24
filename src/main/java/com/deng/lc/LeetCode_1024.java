1df9e7a61c242ec1671440b8d3a9cdbba3f6c956
dyzcs <deng.kinger@gmail.com>
2020-10-24 10:18:15 +0800

    动态规划算法

--- src/main/java/com/deng/lc/LeetCode_1024.java
+++ src/main/java/com/deng/lc/LeetCode_1024.java
@@ -50,6 +50,23 @@ class SolutionLC1024 {
     // 贪心
     public int videoStitching2(int[][] clips, int T) {
         int[] maxn = new int[T];
-        return 0;
+        int last = 0, ret = 0, pre = 0;
+        for (int[] clip : clips) {
+            if (clip[0] < T) {
+                maxn[clip[0]] = Math.max(maxn[clip[0]], clip[1]);
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            last = Math.max(last, maxn[i]);
+            if (i == last) {
+                return -1;
+            }
+            if (i == pre) {
+                ret++;
+                pre = last;
+            }
+        }
+        return ret;
     }
 }
