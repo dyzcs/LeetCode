dcfca396ea84f08aaf60f5dd2d88a95053944f92
dyzcs <deng.kinger@gmail.com>
2020-10-24 09:51:11 +0800

    Update LeetCode_1024.java

--- src/main/java/com/deng/lc/LeetCode_1024.java
+++ src/main/java/com/deng/lc/LeetCode_1024.java
@@ -6,4 +6,7 @@ package com.deng.lc;
  * @see <a href="https://leetcode-cn.com/problems/video-stitching/">1024. 视频拼接</a>
  */
 public class LeetCode_1024 {
+    public static void main(String[] args) {
+        
+    }
 }
