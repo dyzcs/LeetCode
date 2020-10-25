bca22fe72ffaf5edaa0622bdbda70d5cf47d66b8
dyzcs <deng.kinger@gmail.com>
2020-10-25 12:42:30 +0800

    Update LeetCode_845.java

--- src/main/java/com/deng/lc/LeetCode_845.java
+++ src/main/java/com/deng/lc/LeetCode_845.java
@@ -40,6 +40,26 @@ class SolutionLC845 {
     }
 
     public int longestMountain2(int[] A) {
-        return -1;
+        int n = A.length;
+        int ans = 0;
+        int left = 0;
+        while (left + 2 < n) {
+            int right = left + 1;
+            if (A[left] < A[left + 1]) {
+                while (right + 1 < n && A[right] < A[right + 1]) {
+                    right++;
+                }
+                if (right < n - 1 && A[right] > A[right + 1]) {
+                    while (right + 1 < n && A[right] > A[right + 1]) {
+                        right++;
+                    }
+                    ans = Math.max(ans, right - left + 1);
+                } else {
+                    right++;
+                }
+            }
+            left = right;
+        }
+        return ans;
     }
 }
