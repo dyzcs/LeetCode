031eedf7b47b5d7ff45dd938b359c92348e05e40
dyzcs <deng.kinger@gmail.com>
2020-10-25 12:36:00 +0800

    Update LeetCode_845.java

--- src/main/java/com/deng/lc/LeetCode_845.java
+++ src/main/java/com/deng/lc/LeetCode_845.java
@@ -7,17 +7,39 @@ package com.deng.lc;
  */
 public class LeetCode_845 {
     public static void main(String[] args) {
-        int[] arr1 = {2,1,4,7,3,2,5};
-        int[] arr2 = {2,2,2};
+        int[] arr1 = {2, 1, 4, 7, 3, 2, 5};
+        int[] arr2 = {2, 2, 2};
 
         SolutionLC845 solutionLC845 = new SolutionLC845();
-        System.out.println(solutionLC845.longestMountain(arr1));
-        System.out.println(solutionLC845.longestMountain(arr2));
+        System.out.println(solutionLC845.longestMountain1(arr1));
+        System.out.println(solutionLC845.longestMountain2(arr2));
     }
 }
 
 class SolutionLC845 {
-    public int longestMountain(int[] A) {
-        return 0;
+    public int longestMountain1(int[] A) {
+        int n = A.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] left = new int[n];
+        for (int i = 1; i < n; i++) {
+            left[i] = A[i - 1] < A[i] ? left[i - 1] + 1 : 0;
+        }
+        int[] right = new int[n];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = A[i + 1] < A[i] ? right[i + 1] + 1 : 0;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (left[i] > 0 && right[i] > 0) {
+                ans = Math.max(ans, left[i] + right[i] + 1);
+            }
+        }
+        return ans;
+    }
+
+    public int longestMountain2(int[] A) {
+        return -1;
     }
 }
