141cd3117a62c01690fcc87c1609fa765c13fb3d
dyzcs <deng.kinger@gmail.com>
2020-10-25 12:56:06 +0800

    Update LeetCode_846.java

--- src/main/java/com/deng/lc/LeetCode_846.java
+++ src/main/java/com/deng/lc/LeetCode_846.java
@@ -1,5 +1,8 @@
 package com.deng.lc;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by Administrator on 2020/10/25.
  *
@@ -21,6 +24,24 @@ public class LeetCode_846 {
 
 class SolutionLC846 {
     public boolean isNStraightHand(int[] hand, int W) {
-        return false;
+        TreeMap<Integer, Integer> count = new TreeMap<>();
+        for (int card : hand) {
+            count.put(card, count.getOrDefault(card, 1) + 1);
+        }
+        while (count.size() > 0) {
+            int first = count.firstKey();
+            for (int card = first; card < first + W; card++) {
+                if (!count.containsKey(card)) {
+                    return false;
+                }
+                int c = count.get(card);
+                if (c == 1) {
+                    count.remove(card);
+                } else {
+                    count.replace(card, c - 1);
+                }
+            }
+        }
+        return true;
     }
 }
