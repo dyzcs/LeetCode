841ce4e79042c770c3cfc8716daa5619377998bd
dyzcs <deng.kinger@gmail.com>
2020-10-25 12:31:15 +0800

    Update LeetCode_845.java

--- src/main/java/com/deng/lc/LeetCode_845.java
+++ src/main/java/com/deng/lc/LeetCode_845.java
@@ -7,10 +7,17 @@ package com.deng.lc;
  */
 public class LeetCode_845 {
     public static void main(String[] args) {
+        int[] arr1 = {2,1,4,7,3,2,5};
+        int[] arr2 = {2,2,2};
 
+        SolutionLC845 solutionLC845 = new SolutionLC845();
+        System.out.println(solutionLC845.longestMountain(arr1));
+        System.out.println(solutionLC845.longestMountain(arr2));
     }
 }
 
 class SolutionLC845 {
-    
+    public int longestMountain(int[] A) {
+        return 0;
+    }
 }
