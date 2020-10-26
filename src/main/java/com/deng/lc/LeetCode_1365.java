34addb57e9cfe4de6fa88a70514fd559c64921f0
dyzcs <deng.kinger@gmail.com>
2020-10-26 09:40:20 +0800

    Update LeetCode_1365.java

--- src/main/java/com/deng/lc/LeetCode_1365.java
+++ src/main/java/com/deng/lc/LeetCode_1365.java
@@ -5,6 +5,22 @@ package com.deng.lc;
  */
 public class LeetCode_1365 {
     public static void main(String[] args) {
+        int[] arr1 = {8, 1, 2, 2, 3};
+        int[] arr2 = {6, 5, 4, 8};
+        int[] arr3 = {7, 7, 7, 7};
+
         
+    }
+}
+
+class SolutionLC1365 {
+    public int[] smallerNumbersThanCurrent1(int[] nums) {
+        return null;
+    }
+    public int[] smallerNumbersThanCurrent2(int[] nums) {
+        return null;
+    }
+    public int[] smallerNumbersThanCurrent3(int[] nums) {
+        return null;
     }
 }
