d981a125e21d70d8ed860c9899fd3b726617b260
dyzcs <deng.kinger@gmail.com>
2020-10-26 09:43:51 +0800

    Update LeetCode_1365.java

--- src/main/java/com/deng/lc/LeetCode_1365.java
+++ src/main/java/com/deng/lc/LeetCode_1365.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 /**
  * Created by Administrator on 2020/10/26.
+ *
+ * @see <a href="https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/">1365. 有多少小于当前数字的数字</a>
  */
 public class LeetCode_1365 {
     public static void main(String[] args) {
@@ -22,8 +24,20 @@ public class LeetCode_1365 {
 }
 
 class SolutionLC1365 {
+    // 暴力
     public int[] smallerNumbersThanCurrent1(int[] nums) {
-        return null;
+        int n = nums.length;
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int num : nums) {
+                if (num < nums[i]) {
+                    cnt++;
+                }
+            }
+            ret[i] = cnt;
+        }
+        return ret;
     }
 
     public int[] smallerNumbersThanCurrent2(int[] nums) {
