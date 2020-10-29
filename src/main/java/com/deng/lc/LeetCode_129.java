ee6bae390ded761a25a5519ba13796c2bfd0ff81
dyzcs <deng.kinger@gmail.com>
2020-10-29 12:08:15 +0800

    Update LeetCode_129.java

--- src/main/java/com/deng/lc/LeetCode_129.java
+++ src/main/java/com/deng/lc/LeetCode_129.java
@@ -39,6 +39,14 @@ class SolutionLC129 {
     }
 
     private int dfs(TreeNode root, int prevSum) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
     }
 }
