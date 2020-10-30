c78b22add1f2e89f467ae536ec37264ccba85bd5
dyzcs <deng.kinger@gmail.com>
2020-10-30 21:57:44 +0800

    Update LeetCode_463.java

--- src/main/java/com/deng/lc/LeetCode_463.java
+++ src/main/java/com/deng/lc/LeetCode_463.java
@@ -17,8 +17,27 @@ public class LeetCode_463 {
 }
 
 class SolutionLC463 {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
     public int islandPerimeter(int[][] grid) {
-        
-        return 0;
+        int n = grid.length, m = grid[0].length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    int cnt = 0;
+                    for (int k = 0; k < 4; k++) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0) {
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+        }
+        return ans;
     }
 }
