559da0e7a842aa7a01ca625b1f01f130c7057eaa
dyzcs <deng.kinger@gmail.com>
2020-10-30 22:04:04 +0800

    Update LeetCode_463.java

--- src/main/java/com/deng/lc/LeetCode_463.java
+++ src/main/java/com/deng/lc/LeetCode_463.java
@@ -48,14 +48,27 @@ class SolutionLC463 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    ans += dfs(i, j, grid, n ,m);
+                    ans += dfs(i, j, grid, n, m);
                 }
             }
         }
         return ans;
     }
 
-    private int dfs(int i, int j, int[][] grid, int n, int m) {
-        return 0;
+    private int dfs(int x, int y, int[][] grid, int n, int m) {
+        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) {
+            return 1;
+        }
+        if (grid[x][y] == 2) {
+            return 0;
+        }
+        grid[x][y] = 2;
+        int res = 0;
+        for (int i = 0; i < 4; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            res += dfs(tx, ty, grid, n, m);
+        }
+        return res;
     }
 }
