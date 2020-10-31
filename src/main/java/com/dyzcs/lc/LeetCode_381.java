cd195c1cb847833c47e5f054db050347dc641846
dyzcs <deng.kinger@gmail.com>
2020-10-31 21:38:03 +0800

    Update LeetCode_381.java

--- src/main/java/com/dyzcs/lc/LeetCode_381.java
+++ src/main/java/com/dyzcs/lc/LeetCode_381.java
@@ -17,13 +17,17 @@ class RandomizedCollection {
     Map<Integer, Set<Integer>> idx;
     List<Integer> nums;
 
-    /** Initialize your data structure here. */
+    /**
+     * Initialize your data structure here.
+     */
     public RandomizedCollection() {
         idx = new HashMap<>();
         nums = new ArrayList<>();
     }
 
-    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    /**
+     * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+     */
     public boolean insert(int val) {
         nums.add(val);
         Set<Integer> set = idx.getOrDefault(val, new HashSet<>());
@@ -32,13 +36,33 @@ class RandomizedCollection {
         return set.size() == 1;
     }
 
-    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    /**
+     * Removes a value from the collection. Returns true if the collection contained the specified element.
+     */
     public boolean remove(int val) {
-        return false;
+        if (!idx.containsKey(val)) {
+            return false;
+        }
+        Iterator<Integer> it = idx.get(val).iterator();
+        int i = it.next();
+        int lastNum = nums.get(nums.size() - 1);
+        nums.set(i, lastNum);
+        idx.get(val).remove(i);
+        idx.get(lastNum).remove(nums.size() - 1);
+        if (i < nums.size() - 1) {
+            idx.get(lastNum).add(i);
+        }
+        if (idx.get(val).size() == 0) {
+            idx.remove(val);
+        }
+        nums.remove(nums.size() - 1);
+        return true;
     }
 
-    /** Get a random element from the collection. */
+    /**
+     * Get a random element from the collection.
+     */
     public int getRandom() {
-        return 0;
+        return nums.get((int) (Math.random() * nums.size()));
     }
 }
