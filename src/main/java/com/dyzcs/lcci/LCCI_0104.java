eee1bb1d3d58397a03b9c7c9e26eba0cdf3564cf
dyzcs <deng.kinger@gmail.com>
2020-11-03 21:44:15 +0800

    Update LCCI_0104.java

--- src/main/java/com/dyzcs/lcci/LCCI_0104.java
+++ src/main/java/com/dyzcs/lcci/LCCI_0104.java
@@ -7,10 +7,27 @@ package com.dyzcs.lcci;
  */
 public class LCCI_0104 {
     public static void main(String[] args) {
-
+        String s = "tactcoa";
+        SolutionLCCI0104 solutionLCCI0104 = new SolutionLCCI0104();
+        System.out.println(solutionLCCI0104.canPermutePalindrome1(s));
     }
 }
 
 class SolutionLCCI0104 {
-    
+    public boolean canPermutePalindrome1(String s) {
+        int[] arr = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i)]++;
+        }
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                count++;
+            }
+            if (!(count == 0 || count == 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
