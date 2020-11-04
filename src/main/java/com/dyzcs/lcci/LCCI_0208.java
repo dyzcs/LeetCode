f41bf6f9c6f8055406095e83588013ed4a001550
dyzcs <deng.kinger@gmail.com>
2020-11-04 20:36:17 +0800

    Update LCCI_0208.java

--- src/main/java/com/dyzcs/lcci/LCCI_0208.java
+++ src/main/java/com/dyzcs/lcci/LCCI_0208.java
@@ -26,7 +26,7 @@ class SolutionLCCI0208 {
         ListNode fast = head, slow = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
-            fast = fast.next;
+            fast = fast.next.next;
             if (fast == slow) {
                 while (head != slow) {
                     head = head.next;
