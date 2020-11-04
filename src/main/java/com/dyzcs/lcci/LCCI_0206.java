f5ded25369d159bbba73efa073ade0bb771f85c0
dyzcs <deng.kinger@gmail.com>
2020-11-04 19:00:30 +0800

    Create LCCI_0206.java

---
+++ src/main/java/com/dyzcs/lcci/LCCI_0206.java
@@ -0,0 +1,25 @@
+package com.dyzcs.lcci;
+
+/**
+ * Created by Administrator on 2020/11/4.
+ */
+public class LCCI_0206 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class SolutionLCCI0206 {
+    static class ListNode {
+        int val;
+        Solution.ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
