7212f89f99beb3ceab17e3f088461b5a389bf5cf
dyzcs <deng.kinger@gmail.com>
2020-11-04 19:06:41 +0800

    Update LCCI_0206.java

--- src/main/java/com/dyzcs/lcci/LCCI_0206.java
+++ src/main/java/com/dyzcs/lcci/LCCI_0206.java
@@ -25,6 +25,30 @@ class SolutionLCCI0206 {
             slow = slow.next;
             fast = fast.next.next;
         }
-        return false;
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+
+        fast = head;
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
 }
