ecbffa52f2ec792dd69cd320356f5e043c83ca06
dyzcs <deng.kinger@gmail.com>
2020-11-05 15:49:23 +0800

    Create BS_04.java

---
+++ src/main/java/com/leetbook/binarysearch/BS_04.java
@@ -0,0 +1,37 @@
+package com.leetbook.binarysearch;
+
+/**
+ * Created by Administrator on 2020/11/5.
+ */
+public class BS_04 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class SolutionBS04 {
+    public int findMin(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int left = 0, right = nums.length - 1;
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
