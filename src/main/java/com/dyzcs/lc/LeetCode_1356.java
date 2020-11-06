5737b1815cd24226bc3d6ebc6a67baf2c91a27e0
dyzcs <deng.kinger@gmail.com>
2020-11-06 23:09:24 +0800

    Update LeetCode_1356.java

--- src/main/java/com/dyzcs/lc/LeetCode_1356.java
+++ src/main/java/com/dyzcs/lc/LeetCode_1356.java
@@ -1,7 +1,34 @@
 package com.dyzcs.lc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2020/11/6.
  */
 public class LeetCode_1356 {
+}
+
+class SolutionLC1356 {
+    public int[] sortByBits(int[] arr) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int x : arr) {
+            list.add(x);
+        }
+        int[] bit = new int[10001];
+        for (int i = 1; i <= 10000; ++i) {
+            bit[i] = bit[i >> 1] + (i & 1);
+        }
+        list.sort((x, y) -> {
+            if (bit[x] != bit[y]) {
+                return bit[x] - bit[y];
+            } else {
+                return x - y;
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
 }
