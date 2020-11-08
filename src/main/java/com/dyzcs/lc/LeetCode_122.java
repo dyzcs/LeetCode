afd73d86a7ca29b03fccc155ff347e98c45bb374
dyzcs <deng.kinger@gmail.com>
2020-11-08 09:20:34 +0800

    Create LeetCode_122.java

---
+++ src/main/java/com/dyzcs/lc/LeetCode_122.java
@@ -0,0 +1,19 @@
+package com.dyzcs.lc;
+
+/**
+ * Created by Administrator on 2020/11/8.
+ */
+public class LeetCode_122 {
+    public static void main(String[] args) {
+        SolutionLC122 solutionLC122 = new SolutionLC122();
+        System.out.println(solutionLC122.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(solutionLC122.maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(solutionLC122.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
+
+class SolutionLC122 {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+}
