0b37772edebf711e6cb2052813d4681c8f6ca7e0
dyzcs <deng.kinger@gmail.com>
2020-11-08 09:22:07 +0800

    Update LeetCode_122.java

--- src/main/java/com/dyzcs/lc/LeetCode_122.java
+++ src/main/java/com/dyzcs/lc/LeetCode_122.java
@@ -14,6 +14,10 @@ public class LeetCode_122 {
 
 class SolutionLC122 {
     public int maxProfit(int[] prices) {
-        return 0;
+        int ans = 0;
+        for (int i = 1; i < prices.length; i++) {
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans;
     }
 }
