bc31b94748d21396ad9968c31e685f3bffc05f10
dyzcs <deng.kinger@gmail.com>
2020-11-13 22:15:18 +0800

    Update LeetCode_328.java

--- src/main/java/com/dyzcs/lc/LeetCode_328.java
+++ src/main/java/com/dyzcs/lc/LeetCode_328.java
@@ -28,6 +28,18 @@ class Solution {
     }
 
     public ListNode oddEvenList(ListNode head) {
-        return null;
+        if (head == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
     }
 }
