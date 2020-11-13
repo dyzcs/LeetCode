f01d1db12bdbea34dab53ab8d62eaf96ebbe0fc1
dyzcs <deng.kinger@gmail.com>
2020-11-13 22:14:30 +0800

    Create LeetCode_328.java

---
+++ src/main/java/com/dyzcs/lc/LeetCode_328.java
@@ -0,0 +1,33 @@
+package com.dyzcs.lc;
+
+/**
+ * Created by Administrator on 2020/11/13.
+ */
+public class LeetCode_328 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+}
